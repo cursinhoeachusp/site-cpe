ea71d3c4e664f83d47441e568217213a145f6cc4
Roberta Borges <robertav@usp.br>
2021-01-03 00:56:04 -0300

    -

--- sejaProfessor.php
+++ sejaProfessor.php
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-  <title>Seja Professor - CPE</title>
-
-  <!-- Bootstrap CSS -->
-  <link href="css/bootstrap.min.css" rel="stylesheet">
-  <!-- Nosso CSS -->
-  <link rel="stylesheet" href="css/style.css">
-</head>
+<?php include('common/header.html'); ?>
 
 <header class="masthead" style="background-image:url('/img/Fotos/Alunos.jpg');">
     <div class="overlay"></div>
@@ -30,98 +16,6 @@
     </div>
 </header>
 
-<body>
-
-  <div class="corpo">
-
-      <div class="seção">
-
-        <h1 class="titulo-principal">SEJA PROFESSOR</h1>
-
-      <p class="paragrafo">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet congue dolor. Nam varius ex sed orci sagittis fermentum. Sed at imperdiet ante.
-        </p>
-      </div>
-
-
-      <div class="linha"></div>
-
-    <div class="seção">
-      <h2 class="subtitulo">O que esperamos de você</h1>
-      
-        <p class="paragrafo">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet congue dolor. Nam varius ex sed orci sagittis fermentum. Sed at imperdiet ante. Phasellus mollis turpis at semper sollicitudin. Curabitur luctus sem rhoncus quam faucibus feugiat. Praesent elit arcu, aliquet imperdiet arcu non, porta lacinia risus. Nunc non posuere lorem, non molestie est. Pellentesque a augue dolor. 
-        </p>
-      
-      </div>
-
-      <div class="linha"></div>
-
-    <div class="seção">
-      <h2 class="subtitulo">Processo seletivo</h1>
-      
-      <p class="paragrafo">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet congue dolor. Nam varius ex sed orci sagittis fermentum. Sed at imperdiet ante. Phasellus mollis turpis at semper sollicitudin. Curabitur luctus sem rhoncus quam faucibus feugiat. Praesent elit arcu, aliquet imperdiet arcu non, porta lacinia risus. Nunc non posuere lorem, non molestie est. Pellentesque a augue dolor. 
-        </p>
-      </div>
-
-      <div class="setaInteresse"></div>
-      
-      <div class="botoes" id="recuo">
-        <div class="item">
-            <a href="https://cursinhopopularpro.wixsite.com/cursinho/nossa-equipe" target="_blank">
-              <button class="botao manual"><b>Formulário de interesse</b></button>
-            </a>
-        </div>
-      </div>
-      
-      </div>
-
-      <div class="linha"></div>
-
-    <div class="seção">
-      <h2 class="subtitulo">Editais de processos seletivos anteriores</h1>
-      
-        <p class="paragrafo">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet congue dolor. Nam varius ex sed orci sagittis fermentum. Sed at imperdiet ante.
-        </p>
-      </div>
-      
-
-<div class="seçãoRow">
-      <div class="botoes">
-          <div class="item">
-              <a href="" target="_blank">
-            <button class="botaoAnos manualAnos"><b>2018</b></button>
-              </a>
-          </div>
-      </div>
-      
-      <div class="botoes">
-          <div class="item">
-              <a href="" target="_blank">
-                <button class="botaoAnos manualAnos"><b>2019</b></button>
-              </a>
-            </div>
-          </div>
-      
-      <div class="botoes">
-        <div class="item">
-          <a href="" target="_blank">
-            <button class="botaoAnos manualAnos"><b>2020</b></button>
-          </a>
-        </div>
-      </div>
-    </div>
-
-      
-
-</div>
-
-  <script src="js/jquery.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-</body>
+<?php include('common/sejaProfessor.html'); ?>
 
 <?php include('common/footer.html'); ?>
-
-</html>
